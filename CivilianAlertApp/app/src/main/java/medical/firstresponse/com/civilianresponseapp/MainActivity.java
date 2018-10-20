103830afbc0dc5b7a5f646d590aa7f65ae09126c
Patrick Bartman <me@patrickbartman.com>
2018-10-20 11:09:40 -0500

    modified:   CivilianAlertApp/.idea/codeStyles/Project.xml 	modified:   CivilianAlertApp/.idea/misc.xml 	modified:   CivilianAlertApp/app/src/main/AndroidManifest.xml 	modified:   CivilianAlertApp/app/src/main/java/medical/firstresponse/com/civilianresponseapp/MainActivity.java 	modified:   EMTResponseApp/.idea/codeStyles/Project.xml 	modified:   EMTResponseApp/.idea/misc.xml 	deleted:    EMTResponseApp/.idea/vcs.xml 	modified:   EMTResponseApp/app/src/main/AndroidManifest.xml 	modified:   EMTResponseApp/app/src/main/java/medical/firstresponse/com/emtresponseapp/MainActivity.java

--- CivilianAlertApp/app/src/main/java/medical/firstresponse/com/civilianresponseapp/MainActivity.java
+++ CivilianAlertApp/app/src/main/java/medical/firstresponse/com/civilianresponseapp/MainActivity.java
@@ -1,15 +1,23 @@
 package medical.firstresponse.com.civilianresponseapp;
 
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+/**
+ * https://developer.android.com/training/beam-files/send-files#java
+ */
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    NfcAdapter mNfcAdapter;
+    // Flag to indicate that Android Beam is available
+    boolean mAndroidBeamAvailable  = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,47 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        // NFC isn't available on the device
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            /*
+             * Disable NFC features here.
+             * For example, disable menu items or buttons that activate
+             * NFC-related features
+             */
+            // Android Beam file transfer isn't supported
+        } else if (Build.VERSION.SDK_INT <
+                Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            // If Android Beam isn't available, don't continue.
+            mAndroidBeamAvailable = false;
+            /*
+             * Disable Android Beam file transfer features here.
+             */
+            // Android Beam file transfer is available, continue
+        } else {
+            mAndroidBeamAvailable = true;
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+    }
+
+    // List of URIs to provide to Android Beam
+    private Uri[] mFileUris = new Uri[10];
+    /**
+     * Callback that Android Beam file transfer calls to get
+     * files to share
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private class FileUriCallback implements
+            NfcAdapter.CreateBeamUrisCallback {
+        public FileUriCallback() {
+        }
+        /**
+         * Create content URIs as needed to share with another device
+         */
+        @Override
+        public Uri[] createBeamUris(NfcEvent event) {
+            return mFileUris;
+        }
     }
 
     /**
--- EMTResponseApp/app/src/main/java/medical/firstresponse/com/emtresponseapp/MainActivity.java
+++ EMTResponseApp/app/src/main/java/medical/firstresponse/com/emtresponseapp/MainActivity.java
@@ -1,15 +1,25 @@
 package medical.firstresponse.com.emtresponseapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
+import java.io.File;
+
+/**
+ * https://developer.android.com/training/beam-files/receive-files#java
+ */
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    /*static {
-        System.loadLibrary("native-lib");
-    }*/
+    // A File object containing the path to the transferred files
+    private File mParentPath;
+    // Incoming Intent
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,86 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+    }
+
+    /*
+     * Called from onNewIntent() for a SINGLE_TOP Activity
+     * or onCreate() for a new Activity. For onNewIntent(),
+     * remember to call setIntent() to store the most
+     * current Intent
+     *
+     */
+    private void handleViewIntent() {
+        // Get the Intent action
+        mIntent = getIntent();
+        String action = mIntent.getAction();
+        /*
+         * For ACTION_VIEW, the Activity is being asked to display data.
+         * Get the URI.
+         */
+        if (TextUtils.equals(action, Intent.ACTION_VIEW)) {
+            // Get the URI from the Intent
+            Uri beamUri = mIntent.getData();
+            /*
+             * Test for the type of URI, by getting its scheme value
+             */
+            if (beamUri != null) {
+                if (TextUtils.equals(beamUri.getScheme(), "file")) {
+                    mParentPath = handleFileUri(beamUri);
+                } else if (TextUtils.equals(
+                        beamUri.getScheme(), "content")) {
+                    mParentPath = handleContentUri(beamUri);
+                }
+            }
+        }
+    }
+
+    public File handleFileUri(Uri beamUri) {
+        // Get the path part of the URI
+        String fileName = beamUri.getPath();
+        // Create a File object for this filename
+        File copiedFile = new File(fileName);
+        // Get the file's parent directory
+        return copiedFile.getParentFile();
+    }
+
+    public File handleContentUri(Uri beamUri) {             //TODO: was originally a type string but caused errors everywhere. Changed to File but might be wrong??
+        // Position of the filename in the query Cursor
+        int filenameIndex;
+        // File object for the filename
+        File copiedFile;
+        // The filename stored in MediaStore
+        String fileName;
+        // Test the authority of the URI
+        if (!TextUtils.equals(beamUri.getAuthority(), MediaStore.AUTHORITY)) {
+            /*
+             * Handle content URIs for other content providers
+             */
+            // For a MediaStore content URI
+        } else {
+            // Get the column that contains the file name
+            String[] projection = { MediaStore.MediaColumns.DATA };
+            Cursor pathCursor =
+                    getContentResolver().query(beamUri, projection,
+                            null, null, null);
+            // Check for a valid cursor
+            if (pathCursor != null &&
+                    pathCursor.moveToFirst()) {
+                // Get the column index in the Cursor
+                filenameIndex = pathCursor.getColumnIndex(
+                        MediaStore.MediaColumns.DATA);
+                // Get the full file name including path
+                fileName = pathCursor.getString(filenameIndex);
+                // Create a File object for the filename
+                copiedFile = new File(fileName);
+                // Return the parent directory of the file
+                return copiedFile.getParentFile();
+            } else {
+                // The query didn't work; return null
+                return null;
+            }
+        }
+        return null;
     }
 
     /**
