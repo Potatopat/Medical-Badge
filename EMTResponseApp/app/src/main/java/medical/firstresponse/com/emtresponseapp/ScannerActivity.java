5d4a514897f751c9d4551b11d8dbabeae898a5df
boomshakakan <msteve20@uic.edu>
2018-10-20 00:53:48 -0500

    my baby's 1st compile

---
+++ EMTResponseApp/app/src/main/java/medical/firstresponse/com/emtresponseapp/ScannerActivity.java
@@ -0,0 +1,46 @@
+package medical.firstresponse.com.emtresponseapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v("howdy", rawResult.getText()); // Prints scan results
+        Log.v("howdy", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+    }
+}
