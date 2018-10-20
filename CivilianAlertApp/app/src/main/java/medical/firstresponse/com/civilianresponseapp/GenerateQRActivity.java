f6ca8b3857b9ed9a84cae832c78414355f3c15f6
boomshakakan <msteve20@uic.edu>
2018-10-20 12:06:46 -0500

    Included JSON of data and QR Code
    QR Code and JSON moved to resources folder, QR is generated from JSON
    file

---
+++ CivilianAlertApp/app/src/main/java/medical/firstresponse/com/civilianresponseapp/GenerateQRActivity.java
@@ -0,0 +1,29 @@
+package medical.firstresponse.com.civilianresponseapp;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class GenerateQRActivity extends AppCompatActivity {
+
+
+
+}
